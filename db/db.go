5b27a1e97a142129dbcfe28573e05954efa65df4
Michael Emmanuel <michael.emmanuel732@gmail.com>
2026-01-28 17:01:50 -0500

    create user sign up route

--- db/db.go
+++ db/db.go
@@ -28,7 +28,8 @@ func createTables() {
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
 		email TEXT NOT NULL UNIQUE,
 		password TEXT NOT NULL
-	)`
+	)
+	`
 
 	_, err := DB.Exec(createUsersTable)
 
@@ -43,7 +44,7 @@ func createTables() {
 		description TEXT NOT NULL,
 		location TEXT NOT NULL,
 		dateTime DATETIME NOT NULL,
-		user_id INTEGER
+		user_id INTEGER,
 		FOREIGN KEY(user_id) REFERENCES users(id)
 	)
 	`
