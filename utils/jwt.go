f03d75f21c766a5c724a6a2c75152aba33e126e6
Michael Emmanuel <michael.emmanuel732@gmail.com>
2026-01-28 19:19:03 -0500

    test jwt protected routes

--- utils/jwt.go
+++ utils/jwt.go
@@ -26,7 +26,7 @@ func VerifyToken(token string) error {
 		if !ok {
 			return nil, errors.New("Unexpected signing method")
 		}
-		return secretKey, nil
+		return []byte(secretKey), nil
 	})
 
 	if err != nil {
